d4ffffe0da8ad35a9e1ee4c5efd990c2ddd2844b
Kazurov Kostya <kazurovzxc@mail.ru>
2025-01-07 04:14:50 +0200

    Создал контроллеры

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.model.Post;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PostController {
+
+}
