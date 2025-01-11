34fa68620c7a164677516092dd7668a99118c716
Kazurov Kostya <kazurovzxc@mail.ru>
2025-01-11 02:52:23 +0200

    пользовательский контроллер

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class UserController {
+
+
+}
